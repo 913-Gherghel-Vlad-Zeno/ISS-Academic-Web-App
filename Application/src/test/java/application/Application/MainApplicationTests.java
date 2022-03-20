bd0a88310c8c404d381a31067825f8ea4017f194
913 Stefan Alexandru Herlea <herleastefan@yahoo.com>
2022-03-20 14:10:47 +0200

    Main application backend
    Only the hello world program

---
+++ Application/src/test/java/application/Application/MainApplicationTests.java
@@ -0,0 +1,13 @@
+package application.Application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
