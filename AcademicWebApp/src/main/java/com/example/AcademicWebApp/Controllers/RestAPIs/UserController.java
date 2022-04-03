6b47f53dac4c2c32e9c84caddcbd1e32f18add66
913_Ianos_Raluca <raluca.ianos@stud.ubbcluj.ro>
2022-04-03 17:15:33 +0300

    added frontend

--- AcademicWebApp/src/main/java/com/example/AcademicWebApp/Controllers/RestAPIs/UserController.java
+++ AcademicWebApp/src/main/java/com/example/AcademicWebApp/Controllers/RestAPIs/UserController.java
@@ -3,6 +3,8 @@ package com.example.AcademicWebApp.Controllers.RestAPIs;
 
 import com.example.AcademicWebApp.Repositories.UsersRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Locale;
@@ -13,14 +15,15 @@ public class UserController {
     @Autowired
     UsersRepo usersRepo;
 
+    @CrossOrigin(origins = "http://localhost:4200/")
     @GetMapping("/user/{username}")
-    public String getUser(@PathVariable("username") String username){
+    public ResponseEntity<String> getUser(@PathVariable("username") String username){
 
         if(usersRepo.existsById(username)){
-            return usersRepo.getById(username).getRole().toString();
+            return new ResponseEntity<String>(usersRepo.getById(username).getRole().toString(), HttpStatus.OK);
         }
         else{
-            return "null";
+            return new ResponseEntity<String>("null", HttpStatus.OK);
         }
 
     }
