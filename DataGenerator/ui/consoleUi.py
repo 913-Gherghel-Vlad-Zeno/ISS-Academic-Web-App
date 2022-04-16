bdc39f0177fd44e13182bfc5ab514fd910830b01
913 Stefan Alexandru Herlea <herleastefan@yahoo.com>
2022-04-16 20:19:26 +0300

    added a repo class and constructed the faculties dictionary

--- DataGenerator/ui/consoleUi.py
+++ DataGenerator/ui/consoleUi.py
@@ -3,18 +3,24 @@ from exceptions.uiException import UiException
 
 class ConsoleUi:
 
-    def __init__(self):
+    def __init__(self, service):
+        self.__service = service
         self.__noUsers = 0
         self.__noStudents = 0
         self.__noTeachers = 0
         self.__noStaff = 0
-        self.__noFaculties = 0
 
     def __readUsers(self):
         print()
         self.__noUsers = int(input("Enter the number of users: "))
+
         self.__noTeachers = int(input("Enter the number of teachers: "))
+
+        if self.__noTeachers < 10:
+            raise UiException("Too few teachers!")
+
         self.__noStudents = int(input("Enter the number of students: "))
+
         self.__noStaff = self.__noUsers - (self.__noTeachers + self.__noStudents)
 
         if self.__noStaff <= 0:
@@ -22,21 +28,12 @@ class ConsoleUi:
 
         print("Staff members: " + str(self.__noStaff))
 
-    def __readFaculties(self):
-        print()
-        mn = min(17, self.__noTeachers)
-        self.__noFaculties = int(input("Enter the number of faculties(<= " + str(mn) + "): "))
-
-        if self.__noFaculties > mn:
-            raise UiException("Too many faculties entered")
-
     def start(self):
         running = True
 
         while running:
             try:
                 self.__readUsers()
-                self.__readFaculties()
             except UiException as ex:
                 print(ex)
                 continue
