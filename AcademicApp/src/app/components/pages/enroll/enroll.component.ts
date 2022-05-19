e28bc2897986ac5ae042eb026daf41bb2de32957
Alice laptop <53339016+913AliceHincu@users.noreply.github.com>
2022-05-20 01:27:47 +0300

    Finished student menu

--- AcademicApp/src/app/components/pages/enroll/enroll.component.ts
+++ AcademicApp/src/app/components/pages/enroll/enroll.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LOGO_WIDTH, PAGE_PADDING, CONTENT_PADDING } from 'src/app/constants/sizes';
 
 @Component({
   selector: 'app-enroll',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./enroll.component.css']
 })
 export class EnrollComponent implements OnInit {
+  logoWidth = LOGO_WIDTH
+  pagePadding = PAGE_PADDING
+  contentPadding = CONTENT_PADDING
 
   constructor() { }
 
