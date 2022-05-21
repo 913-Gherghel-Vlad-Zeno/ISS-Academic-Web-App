9bb3e9a7bf7526e347321dc6604090ea50be02a5
Alice laptop <53339016+913AliceHincu@users.noreply.github.com>
2022-05-21 23:46:14 +0300

    accidentally worked on main. Profile page ready for backend

--- AcademicApp/src/app/components/pages/profile-page/profile-page.component.ts
+++ AcademicApp/src/app/components/pages/profile-page/profile-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { LOGO_WIDTH, PAGE_PADDING, CONTENT_PADDING } from 'src/app/constants/sizes';
 
 @Component({
@@ -6,14 +7,45 @@ import { LOGO_WIDTH, PAGE_PADDING, CONTENT_PADDING } from 'src/app/constants/siz
   templateUrl: './profile-page.component.html',
   styleUrls: ['./profile-page.component.css']
 })
+
 export class ProfilePageComponent implements OnInit {
   logoWidth = LOGO_WIDTH
   pagePadding = PAGE_PADDING
   contentPadding = CONTENT_PADDING
 
+  namePlaceholder = 'Name...';
+  phonePlaceholder = 'Phone...';
+  emailPlaceholder = 'Email...';
+  cnpPlaceholder = 'Cnp...';
+  homePlaceholder = 'Home...';
+  bursierText: string = 'Not yet decided';
+
+
   constructor() { }
 
   ngOnInit(): void {
+    this.fillForm();
+  }
+
+  onSubmit(f: NgForm) {
+    /**
+     * @TO_DO - add validations + backend
+     */
+    console.log(f.value);  // here you get the values from the from
+    console.log(f.valid);  // check if form is valid
+
+    // here send data to backend
+
+    // TO DO: the modal where you confirm that the data was send + refresh page to see modifications
+  }
+
+  fillForm(){
+    /** 
+     * @TO_DO - Here get from backend to fill the profile.
+     */
+
+    let test_input = "Let's see if it fills..."
+    this.namePlaceholder = test_input;
   }
 
 }
